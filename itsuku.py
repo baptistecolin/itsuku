7d37e81240d82b0d262e0eea65bae3d4fb122e9b
Baptiste <baptiste.colin@outlook.com>
2017-10-11 11:15:14 +0200

    defines parameters

--- itsuku.py
+++ itsuku.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
-bias = 2
+import phis
+from math import log2, floor
+
+bias = 2 # quadratic bias
+n = 2 # number of dependencies
+T = 2**10 # length of the main array
+x = 64 # size of elements in the main array
+L = floor(3.3*log2(T)) # length of one search
+d = 5 # difficulty of the PoW
+P = 1 # number of independent sequences
