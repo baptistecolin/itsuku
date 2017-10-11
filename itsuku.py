b19cdb2b7e313ebd4d086e2ed3732278f14760cb
Baptiste <baptiste.colin@outlook.com>
2017-10-11 15:10:38 +0200

    adds step 1.a of the memory construction

--- itsuku.py
+++ itsuku.py
@@ -15,11 +15,23 @@ d = 5 # difficulty of the PoW
 P = 1 # number of independent sequences
 l=T/P # length of one independent sequence
 
-X = [] # memory
+X = [None]*T # memory
 H = sha512() # hash function
 
 if M == 64:
     I = 'N\x8a\xc3\x9c\x83w\x1e\xd4t\xb6\x90\xb0\x10f\xda\xd5F@f"$\x12\x89\x7fN\xf74\x86\xcf^\xf3/\xbc\x14\xea\xc4\x88w\x04\x0bP\xe4\xa8bL\x95Z)\xf8\x9f\x87\t\x14iR,\x0e\x8e\xdc\xd1\xce^\xc3U'  # initial challenge (randomly generated m bytes array)
 else:
     I = os.urandom(M)
+
+# Step (1)
+# Building a challenge dependent memory
+    
+# Step (1.a)
+for p in range(P):
+    for i in range(n):
+        # TODO: properly turn the int p into a 4 bytes hex string
+        hash_input = str(p) + I
+
+        H.update(hash_input)
+        X[p*l+i] = H.digest()
 
