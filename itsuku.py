af95cd0a65247cb45c3175c669e00b2d8750c98f
Baptiste <baptiste.colin@outlook.com>
2017-10-11 11:45:56 +0200

    removes unvisible characters and imports pyblake2

--- itsuku.py
+++ itsuku.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 import phis
-from math import log2, floor
+from pyblake2 import blake2b
+from math import floor, log
 
 bias = 2 # quadratic bias
 n = 2 # number of dependencies
 T = 2**10 # length of the main array
 x = 64 # size of elements in the main array
-L = floor(3.3*log2(T)) # length of one search
-d = 5 # difficulty of the PoW
+L = floor(3.3*log(T,2)) # length of one search
+d = 5 # difficulty of the PoW
 P = 1 # number of independent sequences
+
