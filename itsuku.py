d242ee888fb6096dd3547249fb766a5353a1cf6a
Baptiste <baptiste.colin@outlook.com>
2017-10-12 12:26:07 +0200

    I disrespected Git best practices so much with this one that I'm unable to come up with a relevant comment

--- itsuku.py
+++ itsuku.py
@@ -2,7 +2,6 @@
 
 import os
 import struct
-from phis import phis
 from hashlib import sha512
 from math import ceil, log
 
@@ -16,26 +15,61 @@ P = 1 # number of independent sequences
 l = ceil(T/P) # length of one independent sequence
 
 X = [None]*T # memory
-H = sha512() # hash function
+HASH = sha512() # hash function
 
 def phi(seed, i, byte_order='big', method='high-level'):
+    # Will only work as expected if the seed is 4 bytes long
+    assert len(seed) == 4
+
     J = int.from_bytes(seed, byte_order)
     R = i-1
     
     if method=='high-level':
-        res = R*(1-((J*J)/(2**64)))
+        res = R*(1-((J*J)//(2**64)))
     else:
         # We are using the operations suggested at page 7 in https://www.cryptolux.org/images/0/0d/Argon2.pdf
-        x = (J**2)/(2**32)
-        y = ((i-1)*x)/(2**32)
+        x = (J**2)//(2**32)
+        y = ((i-1)*x)//(2**32)
         res = (i-1) - 1 - y
     
     return res
 
-def hash_function(h,x):
+def phis(seed,i,n):
+    assert n>=1 and n<=11
+    
+    # The ouput is a list of length n, where the k-th element is phi_{k}(i)
+    res = []
+    res.append(i-1) # phi_{0}(i)
+    if n>=2:
+        phi_i = phi(seed,i)
+        res.append(phi_i) # phi_{1}
+        if n>=3:
+            res.append(phi_i // 2)
+            if n>=4:
+                res.append((i - 1) // 2)
+                if n>=5:
+                    res.append((phi_i + i) // 2)
+                    if n>=6:
+                        res.append(3 * phi_i // 4)
+                        if n>=7:
+                            res.append(3 * i // 4)
+                            if n>=8:
+                                res.append(phi_i // 4)
+                                if n>=9:
+                                    res.append(i // 4)
+                                    if n>=10:
+                                        res.append(phi_i * 7 // 8)
+                                        if n>=11:
+                                            res.append(i * 7 // 8)
+    assert len(res) == n
+    return res
+
+
+def H(M,x):
     # Encapsulate hashing operations such as digest, update ... for better readability
-    h.update(x)
-    return h.digest()
+    HASH.update(x)
+    output = HASH.digest()
+    return output[:M]
 
 # Turns the int 1024 to the byte string b'\x00\x00\x04\x00', that is fit for hashing
 def int_to_4bytes(n):
@@ -46,7 +80,7 @@ if M == 64:
 else:
     I = os.urandom(M)
 
-def memory_build(I, n, P, H):
+def memory_build(I, n, P, M):
     # Step (1)
     # Building a challenge dependent memory
         
@@ -55,26 +89,29 @@ def memory_build(I, n, P, H):
         for i in range(n):
             hash_input = int_to_4bytes(p) + I
     
-            X[p*l+i] = hash_function(H,hash_input)
+            X[p*l+i] = H(M, hash_input)
     
     # Step (1.b)
     for p in range(P):
         for i in range(n,l):
+            # The seed that is used by phi is the 4 first bytes of X[p*l+i-1]
+            seed = X[p*l+i-1][:4]
+
             # computing phi_{k}(i) for all k up until n
-            phis_i = phis(i,n)
+            phis_list = phis(seed,i,n)
     
             # building the input of the hash function
             hash_input = b""
-            for phi in phis_i:
+            for phi in phis_list:
                 hash_input += X[p*l+ phi]
             
             # inserting the computed hash in the array
-            X[p*l+i] = hash_function(H,hash_input)
+            X[p*l+i] = H(M, hash_input)
 
     return X
 
 
-def merkle_tree(I, X, H):
+def merkle_tree(I, X, M):
     # Building the Merkle Tree
     # It will be implemented as an array, each element being a node
     # The ndoe at index i has its left son at index 2i, and its right son at index 2i+1
@@ -82,7 +119,8 @@ def merkle_tree(I, X, H):
     # The leaves of the tree are the elements of X. Thus, MT[-T:] == X. 
     MT = [None]*(2*len(X)-1)
     MT[-T:] = X
+
     return MT
 
-X = memory_build(I, n, P, H)
-print(merkle_tree(I, X, H))
+X = memory_build(I, n, P, M)
+print(merkle_tree(I, X, M))
