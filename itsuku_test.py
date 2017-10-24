4a21845dd18564a3e81ca2e653a75c978935cb11
Baptiste <baptiste.colin@outlook.com>
2017-10-24 21:52:40 +0200

    adds -empty- tests

---
+++ itsuku_test.py
@@ -0,0 +1,37 @@
+import itsuku
+
+def test_phi():
+    # TODO : write test
+    return None
+
+def test_phis():
+    # TODO : write test
+    return None
+
+def test_H():
+    # TODO : write test
+    return None
+
+def test_int_to_4bytes():
+    # TODO : write test
+    return None
+
+def test_memory_build():
+    # TODO : write test
+    return None
+
+def test_merkle_tree():
+    # TODO : write test
+    return None
+
+def test_compute_Y():
+    # TODO : write test
+    return None
+
+def test_opening():
+    # TODO : write test
+    return None
+
+def test_PoW():
+    # TODO : write test
+    return None
---
+++ opening_test.py
@@ -0,0 +1,14 @@
+import opening
+
+def test_opening():
+    # TODO : write test
+    return None
+
+def test_openingForOneArray():
+    # TODO : write test
+    return None
+
+def test_opening_2():
+    # TODO : write test
+    return None
+
