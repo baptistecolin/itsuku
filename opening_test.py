be9a77eae30c01589f1397a898d6cf60aecc5625
Baptiste <baptiste.colin@outlook.com>
2017-11-06 16:46:34 +0100

    first tests for opening

--- opening_test.py
+++ opening_test.py
@@ -1,10 +1,21 @@
-import opening
+from opening import *
 import pytest
 
-@pytest.mark.skip(reason="to be filled")
 def test_opening():
-    # TODO : write test
-    return None
+    # testing on examples that feature 8 leaves
+    T = 8
+
+    left_leaves = [4,5,6,7]
+    right_leaves = [0,1,2,3]
+    half_of_leaves = [2*i for i in range(0,4)]
+
+    left_leaves_expected_opening = [(1,0)]
+    right_leaves_expected_opening = [(1,1)]
+    half_of_leaves_expected_opening = [(3, 2*i+1) for i in range(0,4)]
+
+    assert opening(T, left_leaves) == left_leaves_expected_opening
+    assert opening(T, right_leaves) == right_leaves_expected_opening
+    assert opening(T, half_of_leaves) == half_of_leaves_expected_opening
 
 @pytest.mark.skip(reason="to be filled")
 def test_openingForOneArray():
