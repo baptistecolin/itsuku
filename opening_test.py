1a06ee32a9c8d442a4bcbc3ca99fc11de3ac90c2
Baptiste <baptiste.colin@outlook.com>
2017-11-06 17:09:00 +0100

    tests are more scalable somehow

--- opening_test.py
+++ opening_test.py
@@ -3,24 +3,24 @@ from math import log2
 import pytest
 
 def test_opening():
-    # testing on examples that feature 8 leaves
-    T = 8
-    H = int(log2(T))+1
+    # testing on examples that feature different amount of leaves
+    for T in [4,8,32,1024]:
+        H = int(log2(T))+1
 
-    left_leaves = [i for i in range(T//2,T)]
-    right_leaves = [i for i in range(0,T//2)]
-    half_of_leaves = [2*i for i in range(0,T//2)]
-    all_leaves = [i for i in range(0,T)]
+        left_leaves = [i for i in range(T//2,T)]
+        right_leaves = [i for i in range(0,T//2)]
+        half_of_leaves = [2*i for i in range(0,T//2)]
+        all_leaves = [i for i in range(0,T)]
 
-    left_leaves_expected_opening = [(1,0)]
-    right_leaves_expected_opening = [(1,1)]
-    half_of_leaves_expected_opening = [(H-1, 2*i+1) for i in range(0,T//2)]
-    all_leaves_expected_opening = []
+        left_leaves_expected_opening = [(1,0)]
+        right_leaves_expected_opening = [(1,1)]
+        half_of_leaves_expected_opening = [(H-1, 2*i+1) for i in range(0,T//2)]
+        all_leaves_expected_opening = []
 
-    assert opening(T, left_leaves) == left_leaves_expected_opening
-    assert opening(T, right_leaves) == right_leaves_expected_opening
-    assert opening(T, half_of_leaves) == half_of_leaves_expected_opening
-    assert opening(T, all_leaves) == all_leaves_expected_opening
+        assert opening(T, left_leaves) == left_leaves_expected_opening
+        assert opening(T, right_leaves) == right_leaves_expected_opening
+        assert opening(T, half_of_leaves) == half_of_leaves_expected_opening
+        assert opening(T, all_leaves) == all_leaves_expected_opening
 
 @pytest.mark.skip(reason="to be filled")
 def test_openingForOneArray():
