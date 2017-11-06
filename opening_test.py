2eda0df1e33bd4e9329800e0cf76f601ceb26a14
Baptiste <baptiste.colin@outlook.com>
2017-11-06 18:20:33 +0100

    additional test + comments

--- opening_test.py
+++ opening_test.py
@@ -11,16 +11,19 @@ def test_opening():
         right_leaves = [i for i in range(0,T//2)]
         half_of_leaves = [2*i for i in range(0,T//2)]
         all_leaves = [i for i in range(0,T)]
+        one_leaf = [0]
 
-        left_leaves_expected_opening = [(1,0)]
-        right_leaves_expected_opening = [(1,1)]
-        half_of_leaves_expected_opening = [(H-1, 2*i+1) for i in range(0,T//2)]
-        all_leaves_expected_opening = []
+        left_leaves_expected_opening = [(1,0)] # only one leaf, the right son of the root
+        right_leaves_expected_opening = [(1,1)] # only one leaf, the left son of the root
+        half_of_leaves_expected_opening = [(H-1, 2*i+1) for i in range(0,T//2)] # remaining leaves
+        all_leaves_expected_opening = [] # nothing ! everything can already be computed
+        one_leaf_expected_opening = [(i,1) for i in range(H-1,0,-1)] # comb shaped opening
 
         assert opening(T, left_leaves) == left_leaves_expected_opening
         assert opening(T, right_leaves) == right_leaves_expected_opening
         assert opening(T, half_of_leaves) == half_of_leaves_expected_opening
         assert opening(T, all_leaves) == all_leaves_expected_opening
+        assert opening(T, one_leaf) == one_leaf_expected_opening
 
 @pytest.mark.skip(reason="to be filled")
 def test_openingForOneArray():
