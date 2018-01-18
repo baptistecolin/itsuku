efd9e21de3e8c32c25375e6a2428bc9c960e5831
Baptiste <baptiste.colin@outlook.com>
2018-01-18 11:41:48 +0100

    corrections subsequent to previous fixes, and also overall test strengthening

--- itsuku_test.py
+++ itsuku_test.py
@@ -107,36 +107,36 @@ def test_merkle_tree():
     M = 64
     T = 2**5
     n = 2
-    P = 1
     I = os.urandom(M)
-    l = T//P
     
-    for n in range(2,12): # should work for different values of n
-        X = memory_build(I, T, n, P, M)
+    for P in [1,2,4]:
+        l = T//P
+        for n in range(2,min(12,l)): # should work for different values of n
+            X = memory_build(I, T, n, P, M)
+            
+            MT = merkle_tree(I, X, M)
+    
+            # asserting the length is 2*T-1
+            assert len(MT) == 2*T-1
+            # asserting the end of the MT is actually the hashed original array
+            assert MT[-T:] == [H(M,x) for x in X]
+            # asserting the constructed items are the hash of their sons
+            for i in range(T-1):
+                assert MT[i] == H(M, MT[2*i+1]+MT[2*i+2]+I)
         
-        MT = merkle_tree(I, X, M)
-
-        # asserting the length is 2*T-1
-        assert len(MT) == 2*T-1
-        # asserting the end of the MT is actually the hashed original array
-        assert MT[-T:] == [H(M,x) for x in X]
-        # asserting the constructed items are the hash of their sons
-        for i in range(T-1):
-            assert MT[i] == H(M, MT[2*i+1]+MT[2*i+2]+I)
+        # test on a particular case : if the initial array is constant,
+        # then each "floor" of the merkle tree should be constant
+        X0 = [int_to_4bytes(0)]*T
+        MT0 = merkle_tree(I, X0, M)
     
-    # test on a particular case : if the initial array is constant,
-    # then each "floor" of the merkle tree should be constant
-    X0 = [int_to_4bytes(0)]*T
-    MT0 = merkle_tree(I, X0, M)
-
-    # iterating over the floors
-    for i in range(1,int(log(T,2))):
-        #remembering the value that we expect to find all over the floor
-        value = MT0[(2**i)-1]
-        # iterating inside the floor
-        for j in range((2**i)-1, (2**(i-1))-2):
-            assert MT0[i] == value
-
+        # iterating over the floors
+        for i in range(1,int(log(T,2))):
+            #remembering the value that we expect to find all over the floor
+            value = MT0[(2**i)-1]
+            # iterating inside the floor
+            for j in range((2**i)-1, (2**(i-1))-2):
+                assert MT0[i] == value
+    
 def test_xor():
     assert xor(b"\x00", b"\x00") == b"\x00"
     assert xor(b"\x01", b"\x00") == b"\x01"
@@ -146,29 +146,29 @@ def test_xor():
 def test_compute_Y():
     M = 64
     T = 2**5
-    P = 1
     S = 64
     L = ceil(3.3*log(T,2))
     I = os.urandom(M)
-    l = T//P
     
-    for n in range(2,12): # should work for different values of n
-        X = memory_build(I, T, n, P, M)
-        MT = merkle_tree(I, X, M)
-        PSI = MT[0]
-        N = os.urandom(32) # nounce
+    for P in [1,2,4]:
+        l = T//P
+        for n in range(2,min(12,l)): # should work for different values of n
+            X = memory_build(I, T, n, P, M)
+            MT = merkle_tree(I, X, M)
+            PSI = MT[0]
+            N = os.urandom(32) # nounce
 
-        Y, OMEGA, i = compute_Y(I, X, L, S, N, PSI)
+            Y, OMEGA, i = compute_Y(I, X, L, S, N, PSI)
 
-        # asserting length
-        assert len(Y) == L+1
-        assert len(i) == L
-        # verifying Y[0] is built as expected
-        assert Y[0] == H(S, N + PSI + I)
-        # verifying Y is correctly constructed
-        for j in range(1,L+1):
-            assert i[j-1] == int.from_bytes(Y[j-1], 'big') % T
-            assert Y[j] == H(S, Y[j-1] + xor(X[i[j-1]], I))
+            # asserting length
+            assert len(Y) == L+1
+            assert len(i) == L
+            # verifying Y[0] is built as expected
+            assert Y[0] == H(S, N + PSI + I)
+            # verifying Y is correctly constructed
+            for j in range(1,L+1):
+                assert i[j-1] == int.from_bytes(Y[j-1], 'big') % T
+                assert Y[j] == H(S, Y[j-1] + xor(X[i[j-1]], I))
 
 def test_is_PoW_solved():
     assert is_PoW_solved(b'\x00'*64, b'\x00'*63 + b'\x01') == True
@@ -185,122 +185,125 @@ def test_is_PoW_solved():
 def test_build_L():
     M = 64
     T = 2**5
-    P = 1
     S = 64
     L = ceil(3.3*log(T,2))
     I = os.urandom(M)
-    l = T//P
-    
-    for n in range(2,12): # should work for different values of n
-        X = memory_build(I, T, n, P, M)
-        MT = merkle_tree(I, X, M)
-        PSI = MT[0]
-        N = os.urandom(32) # nounce
-        Y, OMEGA, i = compute_Y(I, X, L, S, N, PSI)
+     
+    for P in [1,2,4]:
+        l = T//P
+        for n in range(2,min(12,l)): # should work for different values of n
+            X = memory_build(I, T, n, P, M)
+            MT = merkle_tree(I, X, M)
+            PSI = MT[0]
+            N = os.urandom(32) # nounce
+            Y, OMEGA, i = compute_Y(I, X, L, S, N, PSI)
 
-        round_L = build_L(i, X, P, n)
-        
-        for i_j in i:
-            assert len(round_L[i_j]) == n
-            p = i_j // l
-            if i_j % l < n:
-                # assert correct construction
-                assert round_L[i_j] == X[p*l:p*l+n]
-                
-                # by construct, X[i_j] should be part of round_L[i_j]
-                assert X[i_j] in round_L[i_j]
+            round_L = build_L(i, X, P, n)
+            
+            for i_j in i:
+                assert len(round_L[i_j]) == n
+                p = i_j // l
+                if i_j % l < n:
+                    # assert correct construction
+                    assert round_L[i_j] == X[p*l:p*l+n]
+                    
+                    # by construct, X[i_j] should be part of round_L[i_j]
+                    assert X[i_j] in round_L[i_j]
 
-                # assert that the elements of round_L are actually computable
-                for k in range(0,n):
-                    stuff_to_hash = int_to_4bytes(k) + int_to_4bytes(p) + I
-                    assert round_L[i_j][k] == H(M, stuff_to_hash)
+                    # assert that the elements of round_L are actually computable
+                    for k in range(0,n):
+                        stuff_to_hash = int_to_4bytes(k) + int_to_4bytes(p) + I
+                        assert round_L[i_j][k] == H(M, stuff_to_hash)
 
-            else:
-                seed = X[i_j-1][:4]
-                # assert correct construction
-                assert round_L[i_j] == [ X[p*l + phi_k_i] for phi_k_i in phis(seed, i_j, n) ]
+                else:
+                    seed = X[i_j-1][:4]
+                    # assert correct construction
+                    assert round_L[i_j] == [ X[p*l + phi_k_i] for phi_k_i in phis(seed, i_j%l, n) ]
 
-                # make sure X[i_j] can be recomputed using the elements of round_L[i_j]
-                hash_input = b""
-                for item in round_L[i_j]:
-                    hash_input += item
-                assert H(M,hash_input) == X[i_j]
+                    # make sure X[i_j] can be recomputed using the elements of round_L[i_j]
+                    hash_input = b""
+                    for item in round_L[i_j]:
+                        hash_input += item
+                    assert H(M,hash_input) == X[i_j]
 
 def test_provided_indexes():
     M = 64
     T = 2**5
-    P = 1
     S = 64
     L = ceil(3.3*log(T,2))
     I = os.urandom(M)
-    l = T//P
-    
-    for n in range(2,12): # should work for different values of n
-        X = memory_build(I, T, n, P, M)
-        MT = merkle_tree(I, X, M)
-        PSI = MT[0]
-        N = os.urandom(32) # nounce
-        Y, OMEGA, i = compute_Y(I, X, L, S, N, PSI)
-        round_L = build_L(i, X, P, n)
+     
+    for P in [1,2,4]:
+        l = T//P
+        for n in range(2,min(12,l)): # should work for different values of n
+            X = memory_build(I, T, n, P, M)
+            MT = merkle_tree(I, X, M)
+            PSI = MT[0]
+            N = os.urandom(32) # nounce
+            Y, OMEGA, i = compute_Y(I, X, L, S, N, PSI)
+            round_L = build_L(i, X, P, n)
 
-        indexes = provided_indexes(round_L, P, T, n)
+            indexes = provided_indexes(round_L, P, T, n)
 
-        for i_j in i:
-            # all the i_j should be in indexes as X[i_j] can alwas be recomputed
-            # using only the elements of round_L[i_j]. Thus, X[i_j] can and must
-            # be considered as a given if round_L is known
-            assert i_j in indexes
-            
-            p = i_j // l
-            
-            if i_j % l < n :
-                # case where the elements of round[i_j] were computed at step (1.a)
-                for k in range(p*l, p*l+n):
-                    assert k in indexes
-            else :
-                # case where the elements of round[i_j] were computed at step (1.b)
+            for index in indexes:
+                assert index < T
 
-                seed = round_L[i_j][0][:4]
+            for i_j in i:
+                # all the i_j should be in indexes as X[i_j] can alwas be recomputed
+                # using only the elements of round_L[i_j]. Thus, X[i_j] can and must
+                # be considered as a given if round_L is known
+                assert i_j in indexes
+                
+                p = i_j // l
+                
+                if i_j % l < n :
+                    # case where the elements of round[i_j] were computed at step (1.a)
+                    for k in range(p*l, p*l+n):
+                        assert k in indexes
+                else :
+                    # case where the elements of round[i_j] were computed at step (1.b)
 
-                # The seed computed using round_L should be the same seed as the
-                # one used during the construction of X
-                assert seed == X[i_j - 1][:4]
-                for index in [p*l+phi_k_i for phi_k_i in phis(seed, i_j, n)]:
-                    assert index in indexes
-            
-        # Asserting there are no duplicates
-        assert len(indexes) == len(set(indexes))
+                    seed = round_L[i_j][0][:4]
+
+                    # The seed computed using round_L should be the same seed as the
+                    # one used during the construction of X
+                    assert seed == X[i_j - 1][:4]
+                    for index in [p*l+phi_k_i for phi_k_i in phis(seed, i_j % l, n)]:
+                        assert index in indexes
+                
+            # Asserting there are no duplicates
+            assert len(indexes) == len(set(indexes))
 
 def test_build_Z():
     M = 64
     T = 2**5
-    P = 1
     S = 64
     L = ceil(3.3*log(T,2))
     I = os.urandom(M)
-    l = T//P
     
-    for n in range(2,12): # should work for different values of n
-        X = memory_build(I, T, n, P, M)
-        MT = merkle_tree(I, X, M)
-        PSI = MT[0]
-        N = os.urandom(32) # nounce
-        Y, OMEGA, i = compute_Y(I, X, L, S, N, PSI)
-        round_L = build_L(i, X, P, n)
-        
-        # A shift has to be applied so the indexes match those of the
-        # Merkle Tree and not those of X.
-        indexes = [ index + T - 1 for index in  provided_indexes(round_L, P, T, n)]
+    for P in [1,2,4]:
+        l = T//P
+        for n in range(2,min(12,l)): # should work for different values of n
+            X = memory_build(I, T, n, P, M)
+            MT = merkle_tree(I, X, M)
+            PSI = MT[0]
+            N = os.urandom(32) # nounce
+            Y, OMEGA, i = compute_Y(I, X, L, S, N, PSI)
+            round_L = build_L(i, X, P, n)
+            
+            # A shift has to be applied so the indexes match those of the
+            # Merkle Tree and not those of X.
+            indexes = [ index + T - 1 for index in  provided_indexes(round_L, P, T, n)]
 
-        Z = build_Z(round_L, MT, P, T, n)
+            Z = build_Z(round_L, MT, P, T, n)
 
-        for k in Z:
-            assert k not in indexes
-            assert Z[k] == MT[k]
-            if k >= T-1:
-                assert Z[k] == H( M, X[k-(T-1)] )
-        
-        assert set(Z.keys()) == set(opening(T, provided_indexes(round_L, P, T, n)))
+            for k in Z:
+                assert k not in indexes
+                assert Z[k] == MT[k]
+                if k >= T-1:
+                    assert Z[k] == H( M, X[k-(T-1)] )
+            
+            assert set(Z.keys()) == set(opening(T, provided_indexes(round_L, P, T, n)))
 
 
 @pytest.mark.skip(reason="to be filled")
