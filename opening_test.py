17523195019b95394ffaeebf9ecfaeb3b4e33d79
Baptiste <baptiste.colin@outlook.com>
2018-02-05 09:16:39 +0100

    fixes opening

--- opening_test.py
+++ opening_test.py
@@ -15,18 +15,21 @@ def test_opening():
         half_of_leaves = [2*i for i in range(0,T//2)]
         all_leaves = [i for i in range(0,T)]
         one_leaf = [0]
+        empty = []
 
         left_leaves_expected_opening = [(1,0)] # only one leaf, the right son of the root
         right_leaves_expected_opening = [(1,1)] # only one leaf, the left son of the root
         half_of_leaves_expected_opening = [(H-1, 2*i+1) for i in range(0,T//2)] # remaining leaves
         all_leaves_expected_opening = [] # nothing ! everything can already be computed
         one_leaf_expected_opening = [(i,1) for i in range(H-1,0,-1)] # comb shaped opening
-
+        empty_expected_opening = [(H-1,t) for t in range(T)] # all the leaves
+        
         assert opening(T, left_leaves) == left_leaves_expected_opening
         assert opening(T, right_leaves) == right_leaves_expected_opening
         assert opening(T, half_of_leaves) == half_of_leaves_expected_opening
         assert opening(T, all_leaves) == all_leaves_expected_opening
         assert opening(T, one_leaf) == one_leaf_expected_opening
+        assert opening(T, empty) == empty_expected_opening
 
 def test_openingForOneArray():
     # testing for the same examples as above
@@ -38,18 +41,21 @@ def test_openingForOneArray():
         half_of_leaves = [2*i for i in range(0,T//2)]
         all_leaves = [i for i in range(0,T)]
         one_leaf = [0]
+        empty = []
 
         left_leaves_expected_opening = [1] # only one leaf, the right son of the root
         right_leaves_expected_opening = [2] # only one leaf, the left son of the root
         half_of_leaves_expected_opening = [2**(H-1)-1 + 2*i+1 for i in range(0,T//2)] # remaining leaves
         all_leaves_expected_opening = [] # nothing ! everything can already be computed
         one_leaf_expected_opening = [2**i for i in range(H-1,0,-1)] # comb shaped opening
+        empty_expected_opening = [t+T-1 for t in range(T)] # all the leaves
 
         assert openingForOneArray(T, left_leaves) == left_leaves_expected_opening
         assert openingForOneArray(T, right_leaves) == right_leaves_expected_opening
         assert openingForOneArray(T, half_of_leaves) == half_of_leaves_expected_opening
         assert openingForOneArray(T, all_leaves) == all_leaves_expected_opening
         assert openingForOneArray(T, one_leaf) == one_leaf_expected_opening
+        assert openingForOneArray(T, empty) == empty_expected_opening
         
         # The opening + the initial leaves should be enough to enable us to compute the merkle tree root
         # Therefore, the following instructions shouldn't fail
